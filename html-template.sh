4b171ce08c0d4115199ddb943d4d56941825a5b7
R3dHand <corey.oneill87@gmail.com>
2019-05-26 08:43:12 -0700

    upate to instal node-sass .scss

--- html-template.sh
+++ html-template.sh
@@ -57,9 +57,9 @@ echo "" >> index.html
 #
 echo "    <!-- jQuery first, then Popper.js, then Bootstrap JS. -->" >> index.html
 echo "    <!-- build:js js/main.js -->" >> index.html
-echo "    <script src=\"node_modules/jquery/dist/jquery.slim.min.js\"></script>" >> index.html
-echo "    <script src=\"node_modules/popper.js/dist/umd/popper.min.js\"></script>" >> index.html
-echo "    <script src=\"node_modules/bootstrap/dist/js/bootstrap.min.js\"></script>" >> index.html
+echo "    <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>" >> index.html
+echo "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>" >> index.html
+echo "    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>" >> index.html
 echo "    <script src=\"js/scripts.js\"></script>" >> index.html
 echo "    <!-- endbuild -->" >> index.html
 
--- node_setup.sh
+++ node_setup.sh
@@ -3,13 +3,14 @@ echo "running node_setup..."
 
 cmd.exe /c npm init
 cmd.exe /c npm install lite-server --save-dev
+cmd.exe /c npm install --save-dev node-sass@4.9.3
 echo "node_modules" >> .gitignore
 
 #add necessary lines to package.json
 if [[ -f "temp.json" ]]; then
     rm temp.json
 fi
-sed '/scripts/ a\    "start": "npm run lite",\n    "lite": "lite-server",' <package.json >temp.json
+sed '/scripts/ a\    "start": "npm run lite",\n    "lite": "lite-server\n    "scss": "node-sass -o css/ css/",' <package.json >temp.json
 printf "%s" "$(cat temp.json)" > package.json
 
 
@@ -30,7 +31,11 @@ if [[ -d "./css" ]];then
 else
     mkdir css
     echo "" > css/styles.css
+    #setup project to use scss
+    echo "" > css/styles.scss
     echo "creating ./css"
+    echo "CONVERT -- .scss --> .css"
+    cmd.exe /c npm run scss
 fi
 
 #java script
